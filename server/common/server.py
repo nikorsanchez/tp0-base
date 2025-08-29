b652c9f87a67f312bb7568a43279de360e99aff3
niko <nrsanchez@fi.uba.ar>
2025-08-29 03:57:50 -0300

    fix server typo

--- server/common/server.py
+++ server/common/server.py
@@ -19,7 +19,7 @@ class Server:
 
     def _handle_signal(self, signum, frame):
         # Handle termination signals
-        logging.info(f"action: received_signal | signal: {signum} | result: initiating_graceful_shutdown")
+        logging.info(f"action: received_signal | signal: {signum} | result: in_progress")
         self._shutdown_requested = True
         self._graceful_shutdown()
 
