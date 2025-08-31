947d5dd933ba19110867eafd95deab810839f490
niko <nrsanchez@fi.uba.ar>
2025-08-30 22:06:17 -0300

    fix log descr

--- server/common/server.py
+++ server/common/server.py
@@ -84,7 +84,7 @@ class Server:
                         message['number']
                     )
                     store_bets([bet])
-                    logging.info(f"action: apuesta_almacenada | result: success | document: {bet.document}")
+                    logging.info(f"action: apuesta_almacenada | result: success | dni: {bet.document}")
                     protocol.send_confirmation()
                 except Exception as e:
                     logging.error(f"action: apuesta_almacenada | result: fail | error: {e}")
