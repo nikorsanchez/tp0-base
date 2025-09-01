2184ce5f010d2dde825f9b2d7d568d03eac0b2c2
niko <nrsanchez@fi.uba.ar>
2025-09-01 00:17:40 -0300

    docs: fix documentation

--- client/bets/protocol/protocol_consts.go
+++ client/bets/protocol/protocol_consts.go
@@ -3,6 +3,7 @@ package protocol
 const (
     HeaderTypeBet      = 0x01
     HeaderTypeConfirm  = 0x02
+	HeaderTypeFailure  = 0x03
     HeaderSize         = 3 // 2 bytes length + 1 byte type
     MaxMessageSize     = 0xFFFF // Max 65535 bytes
     FieldSeparator     = '|'
